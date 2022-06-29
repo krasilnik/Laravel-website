8cffeab03e92125436d0b13c752a972aff261f23
krasilnik <90803029+krasilnik@users.noreply.github.com>
2022-06-29 19:11:33 +0300

    Update Database.php

--- Laravel/yumukr/app/Models/Database.php
+++ Laravel/yumukr/app/Models/Database.php
@@ -2,16 +2,13 @@
 
 namespace App\Models;
 
-use App\Models\Article;
 
-class Database {
-    public static function get_article_by_id(int $id) {
-       $article = array(
-           100 => new Article(100, 'Матсоні', 'Грузинський must try: хачапурі по-мегрельськи — 132 грн;
-                                               хачапурі по-аджарськи — 119 грн;
-                                               хінкалі класичні — 86 грн/4 шт;
-                                               шашлик зі свинини — 98 грн/100 г
-                                               Вах-вах: хачапурі-човник з курячим філе в горіховому соусі — 119 грн', '/img/matsoni.svg', 'вул. Велика Васильківська, 114;', '(050) 108 28 38'));
-       return $article[$id];
-    }
+use Illuminate\Support\Facades\DB;
+use App\Models\Article;
+class DataBase {
+  public static function get_article_by_id($id) {
+        $article = DB::select("select * from articles where article_id = '$id'");
+        return $article[0];
+        }
 }
+
